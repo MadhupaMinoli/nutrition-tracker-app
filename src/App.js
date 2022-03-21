3ba11e2f37c78f12c96063b00c392854759be2f2
MadhupaMinoli <mmadhupa@gmail.com>
2022-03-21 20:47:10 +0530

    Added auth in aws

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import './App.css';
 import { UserContexProvider } from './store/user-contex';
 import NavBar from './widgets/NavBar';
 import TodayView from './widgets/TodayView';
+import { Amplify, Auth } from 'aws-amplify';
+import awsConfig from './aws-exports';
+import awsExports from './aws-exports';
+import { withAuthenticator, AmplifySignOut, Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { PropaneSharp } from '@mui/icons-material';
+
+Amplify.configure(awsConfig);
+Auth.configure(awsConfig);
 
 function App() {
   return (
-    <div className="App">
+		<Authenticator>
+		{({ signOut, user }) => (
+			<div className="App">
 			<UserContexProvider>
 				<NavBar signOutFunc={signOut}/>
 				<TodayView/>
 			</UserContexProvider>
-    </div>
+			</div>
+		)}
+	</Authenticator>
   );
 }
 
-export default App;
+export default (App);
